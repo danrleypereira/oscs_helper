19b4850c4a5d9e67abd91ba6feb9ab83bfe79b3b
danrleypereira <danrleywillian@gmail.com>
2023-02-19 02:04:05 -0300

    django project with event get api and event admin handle

---
+++ back/events/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    startDate = models.DateTimeField(null=True)
+    endDate = models.DateTimeField(null=True)
+
---
+++ back/events/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+from django.conf.urls import include
+from django.contrib.auth.models import User
+
+from .models import Event
+
+
+from . import views
+
+from rest_framework import routers, serializers, viewsets
+from rest_framework.response import Response
+
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['title', 'description', 'startDate', 'endDate']
+
+class EventViewSet(viewsets.ViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer(queryset, many=True)
+
+    def list(self, request):
+        return Response(self.serializer_class.data)
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'events', EventViewSet)
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('', include(router.urls)),
+]
